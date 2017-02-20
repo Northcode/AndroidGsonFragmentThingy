0a7f15cd1226bb72527d9f3962feb0e17fc5a439
Andreas Larsen <northcode.no@gmail.com>
2017-02-20 17:24:06 +0100

    Added landscape mode!

--- app/src/main/java/no/northcode/jsondata/PersonFragment.java
+++ app/src/main/java/no/northcode/jsondata/PersonFragment.java
@@ -1,96 +1,50 @@
 package no.northcode.jsondata;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.CallSuper;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+/**
+ * Created by andreas on 20/02/17.
+ */
 
-import org.json.JSONArray;
+public class PersonFragment extends Fragment {
 
-import java.util.ArrayList;
-
-public class PersonFragment extends ListFragment {
-
-    ArrayList<Person> person_array;
-    PersonAdapter adapter;
+    Person toDisplay;
 
     public PersonFragment() {
         super();
-        person_array = new ArrayList<>();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        adapter = new PersonAdapter(getContext(), R.layout.fragment_person, person_array);
-        setListAdapter(adapter);
-        return inflater.inflate(R.layout.fragment_person_list, container, false);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onCreate(Bundle savedInst) {
+        super.onCreate(savedInst);
 
-        getUserData();
+        Bundle bundle = getArguments();
+
+        PersonParccel pack = bundle.getParcelable("person");
+        toDisplay = pack.p;
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        Intent openintent = new Intent(this.getActivity(), PersonDetailActivity.class);
-        Person person = (Person)v.getTag();
-        PersonParccel pack = new PersonParccel(person);
-        openintent.putExtra("person", pack);
-        startActivity(openintent);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInst) {
+        View view = inflater.inflate(R.layout.details_person, container, false);
 
-    public void getUserData() {
+        TextView txt_uname = (TextView) view.findViewById(R.id.txt_uname);
+        TextView txt_fname = (TextView) view.findViewById(R.id.txt_fname);
+        TextView txt_lname = (TextView) view.findViewById(R.id.txt_lname);
 
-        final String karkurl = "http://kark.hin.no:8088/d3330log_backend/getTestUsers";
+        if (toDisplay != null) {
+            txt_uname.setText(toDisplay.getUserName());
+            txt_fname.setText(toDisplay.getFirstname());
+            txt_lname.setText(toDisplay.getLastname());
+        }
 
-        System.out.println(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!11 -------------------------- CALLLLLLLLLLLLLLL !!!!!!!!!!!!!!!!!!!!!1");
-
-        RequestQueue rqueue = Volley.newRequestQueue(getContext());
-        JsonArrayRequest request = new JsonArrayRequest(karkurl,
-                new Response.Listener<JSONArray>() {
-
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        Gson gson = new Gson();
-                        person_array = gson.fromJson(response.toString(), new TypeToken<ArrayList<Person>>() {}.getType());
-                        person_array.addAll(person_array);
-                        person_array.addAll(person_array);
-                        person_array.addAll(person_array);
-                        adapter = new PersonAdapter(getContext(), R.layout.fragment_person, person_array);
-                        setListAdapter(adapter);
-                        System.out.println("Got response, should be visible");
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("there was an error downloading: " + error.toString());
-                    }
-                }
-        );
-
-        rqueue.add(request);
-        rqueue.start();
-
+        return view;
     }
 
 }
---
+++ app/src/main/java/no/northcode/jsondata/PersonListFragment.java
@@ -0,0 +1,113 @@
+package no.northcode.jsondata;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.CallSuper;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.support.v4.os.EnvironmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class PersonListFragment extends ListFragment {
+
+    ArrayList<Person> person_array;
+    PersonAdapter adapter;
+
+    public PersonListFragment() {
+        super();
+        person_array = new ArrayList<>();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        adapter = new PersonAdapter(getContext(), R.layout.fragment_person, person_array);
+        setListAdapter(adapter);
+        return inflater.inflate(R.layout.fragment_person_list, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getUserData();
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Person person = (Person) v.getTag();
+        PersonParccel pack = new PersonParccel(person);
+
+        boolean isLandscape = getResources().getConfiguration().orientation == getResources().getConfiguration().ORIENTATION_LANDSCAPE;
+
+        if (isLandscape) {
+            PersonFragment personFragment = new PersonFragment();
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("person", pack);
+            personFragment.setArguments(bundle);
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.layout_details_container, personFragment);
+            // transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            Intent openintent = new Intent(this.getActivity(), PersonDetailActivity.class);
+            openintent.putExtra("person", pack);
+            startActivity(openintent);
+        }
+    }
+
+    public void getUserData() {
+
+        final String karkurl = "http://kark.hin.no:8088/d3330log_backend/getTestUsers";
+
+        System.out.println(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!11 -------------------------- CALLLLLLLLLLLLLLL !!!!!!!!!!!!!!!!!!!!!1");
+
+        RequestQueue rqueue = Volley.newRequestQueue(getContext());
+        JsonArrayRequest request = new JsonArrayRequest(karkurl,
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Gson gson = new Gson();
+                        person_array = gson.fromJson(response.toString(), new TypeToken<ArrayList<Person>>() {}.getType());
+                        person_array.addAll(person_array);
+                        person_array.addAll(person_array);
+                        person_array.addAll(person_array);
+                        adapter = new PersonAdapter(getContext(), R.layout.fragment_person, person_array);
+                        setListAdapter(adapter);
+                        System.out.println("Got response, should be visible");
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("there was an error downloading: " + error.toString());
+                    }
+                }
+        );
+
+        rqueue.add(request);
+        rqueue.start();
+
+    }
+
+}
